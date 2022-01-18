6fe8371d29246723fecffd42fe2aee63ecacab42
dhaoui aziz <dhaouiaziz13@gmail.com>
2022-01-18 15:18:02 +0100

    added a sub doc

--- models/message.js
+++ models/message.js
@@ -1,7 +1,15 @@
 const { Schema, model } = require("mongoose");
-
+const msg =new Schema({
+  body:String,
+  sendername:String,
+  receivername:String,
+  createdat: {
+    type: Date,
+    default: Date.now(),
+  },
+})
 const messageSchema = new Schema({
-  messages: { type: Array, default: [] },
+  messages: { type: [msg], default: [] },
   senderid: Schema.Types.ObjectId,
   receiverid: Schema.Types.ObjectId,
   createdat: {
